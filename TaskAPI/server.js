ad105398903af29704c796610534fa67fcc9210e
Kashaer <kaiser.kaiser211@gmail.com>
2018-02-16 14:01:56 -0500

    initial commit

---
+++ TaskAPI/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8000;
+
+// middleware
+app.use(bodyParser.json())
+// app.use(bodyParser.urllencoded())
+
+// mongoose file
+require('./server/config/mongoose');
+
+// Routes
+require('./server/config/routes')(app);
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
